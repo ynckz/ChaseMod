f795d775e855ce301a02fb95e82b499cd95215f5
ynckz <127309355+ynckz@users.noreply.github.com>
2026-01-19 19:05:33 +0300

    Update ChaseModConfig.cs

--- ChaseModConfig.cs
+++ ChaseModConfig.cs
@@ -18,19 +18,19 @@ public sealed class ChaseModConfig : BasePluginConfig
     [JsonPropertyName("enableKnifeHook")] public bool EnableKnifeHook { get; set; } = true;
     [JsonPropertyName("knifeDamageModify")] public bool KnifeDamageModify { get; set; } = true;
     [JsonPropertyName("knifeDamage")] public int KnifeDamage { get; set; } = 50;
-    [JsonPropertyName("knifeCooldown")] public float KnifeCooldown { get; set; } = 2.0f;
+    [JsonPropertyName("knifeCooldown")] public float KnifeCooldown { get; set; } = 5.0f;
     [JsonPropertyName("stunThrowTime")] public float StunThrowTime { get; set; } = 2.0f;
-    [JsonPropertyName("stunFreezeTime")] public float StunFreezeTime { get; set; } = 15.0f;
+    [JsonPropertyName("stunFreezeTime")] public float StunFreezeTime { get; set; } = 4.0f;
     [JsonPropertyName("stunFreezeRadius")] public float StunFreezeRadius { get; set; } = 500f;
     [JsonPropertyName("stunSameTeam")] public bool StunSameTeam { get; set; } = false;
 
     [JsonPropertyName("enableTeamSwitchingConditions")] public bool EnableTeamSwitchingConditions { get; set; } = true;
-    [JsonPropertyName("maxTerroristWinStreak")] public int MaxTerroristWinStreak { get; set; } = 5;
+    [JsonPropertyName("maxTerroristWinStreak")] public int MaxTerroristWinStreak { get; set; } = 3;
 
     [JsonPropertyName("enableKnifeDisabling")] public bool EnableKnifeDisabling { get; set; } = true;
     [JsonPropertyName("alwaysDisableTerroristKnife")] public bool AlwaysDisableTerroristKnife { get; set; } = false;
 
-    [JsonPropertyName("ctStartFreezeTime")] public float RoundStartFreezeTime { get; set; } = 15.0f;
+    [JsonPropertyName("ctStartFreezeTime")] public float RoundStartFreezeTime { get; set; } = 10.0f;
     [JsonPropertyName("enableFreezeTimeCountDownSound")] public bool EnableFreezeTimeCountDownSound { get; set; } = false;
     [JsonPropertyName("freezeTimeCountDownSoundPath")] public string FreezeTimeCountDownSoundPath { get; set; } = "sounds/player/playerping";
 
